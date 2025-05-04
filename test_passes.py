cf5a43ee93e94ef523d8c3951091f48afd164884
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2025-05-04 23:51:45 +0700

    add multi-fold test case

--- test_passes.py
+++ test_passes.py
@@ -1,3 +1,5 @@
+import pytest
+
 from ir import Value, Constant, Operation, Block
 from ir import bb_to_str
 from passes import constfold
@@ -14,3 +16,17 @@ def test_constfold_simple():
     assert bb_to_str(opt_bb, "optvar") == """\
 optvar0 = getarg(0)
 optvar1 = add(9, optvar0)"""
+
+
+@pytest.mark.xfail
+def test_constfold_multifold():
+    bb = Block()
+    var0 = bb.getarg(0)
+    var1 = bb.add(5, 4)     # this is folded
+    var2 = bb.add(var1, 10) # this should be folded, too
+    var3 = bb.add(var2, var0)
+
+    opt_bb = constfold(bb)
+    assert bb_to_str(opt_bb, "optvar") == """\
+optvar0 = getarg(0)
+optvar1 = add(19, optvar0)"""
