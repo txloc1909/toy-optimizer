8fc2cd5b07883e65cf69952452228ab34a827818
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2025-05-04 23:48:47 +0700

    rewrite constfold in match-case style

--- passes.py
+++ passes.py
@@ -5,13 +5,17 @@ def constfold(bb):
     opt_bb = Block()
 
     for op in bb:
-        if op.name == "add":
-            arg0, arg1 = op.args
-            if isinstance(arg0, Constant) and isinstance(arg1, Constant):
-                value = arg0.value + arg1.value
-                op.make_equal_to(Constant(value))
-                continue
-
-        opt_bb.append(op)
+        match op.name:
+            case "add":
+                arg0, arg1 = op.args
+                if isinstance(arg0, Constant) and isinstance(arg1, Constant):
+                    # fold
+                    value = arg0.value + arg1.value
+                    op.make_equal_to(Constant(value))
+                    continue
+                else: 
+                    opt_bb.append(op)
+            case _:
+                opt_bb.append(op)
 
     return opt_bb
