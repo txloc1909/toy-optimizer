b9595b567cb40dbc0d598d42f79bbdd448ca8b36
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2025-05-17 22:50:35 +0700

    stringify knownbits

--- knownbits.py
+++ knownbits.py
@@ -34,3 +34,45 @@ class KnownBits:
     def contains(self, value: int) -> bool:
         """check whether this instance contains the concrete int `value`"""
         return value & self.knowns == self.ones
+
+    def __repr__(self):
+        if self.is_constant():
+            return f"KnownBits.from_constant({self.ones})"
+        return f"KnownBits({self.ones}, {self.unknowns})"
+
+    def __str__(self):
+        res = []
+        ones, unknowns = self.ones, self.unknowns
+
+        # construct from right to left
+        while True:
+            if not ones and not unknowns:
+                break   # reached leading known 0s
+
+            if ones == -1 and not unknowns:
+                # -1 in two's complement -> all leading bits are 1
+                res.append("1")
+                res.append("...")
+                break
+
+            if unknowns == -1: 
+                # -1 in two's complement -> all leading bits are unknown
+                assert not ones
+                res.append("?")
+                res.append("...")
+                break
+
+            if unknowns & 1:
+                res.append("?")
+            elif ones & 1:
+                res.append("1")
+            else:
+                res.append("0")
+
+            ones >>= 1 
+            unknowns >>= 1
+
+        if len(res) == 0:
+            res = ["0"]
+
+        return "".join(reversed(res))
