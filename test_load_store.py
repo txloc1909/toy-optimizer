e4e74c5247b6e24a529d1a80062dc79d3464bd30
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2026-06-03 21:24:39 +0700

    test: a write doesnt invalidate a caching load

--- test_load_store.py
+++ test_load_store.py
@@ -44,3 +44,24 @@ var5 = escape(var3)
     """
 
     assert bb_to_str(opt_bb).strip() == expected.strip()
+
+def test_store_to_same_object_offset_doesnt_invalidates_load():
+    bb = Block()
+    var0 = bb.getarg(0)
+    var1 = bb.load(var0, 0)
+    var2 = bb.store(var0, 3, 5)
+    var3 = bb.load(var0, 0)
+    bb.escape(var1)
+    bb.escape(var3)
+
+    opt_bb = optimize_load_store(bb)
+
+    expected = """
+var0 = getarg(0)
+var1 = load(var0, 0)
+var2 = store(var0, 3, 5)
+var3 = escape(var1)
+var4 = escape(var1)
+    """
+
+    assert bb_to_str(opt_bb).strip() == expected.strip()
